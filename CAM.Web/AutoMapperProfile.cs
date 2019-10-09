1ae594b921dcf27999df5e75e5617f3527e11167
Perustaja <chrishansen1111@gmail.com>
2019-10-09 11:42:28 -0600

    Changing architecture for a normal Core/Infra/Web structure

--- CAM.Web/AutoMapperProfile.cs
+++ CAM.Web/AutoMapperProfile.cs
@@ -1,6 +1,5 @@
 using AutoMapper;
 using CAM.Core.Entities;
-using CAM.Web.ApiModels;
 
 namespace CAM.Web
 {
@@ -8,21 +7,6 @@ namespace CAM.Web
     {
         public AutoMapperProfile()
         {
-            // Aircraft -> AircraftDto
-            CreateMap<Aircraft, AircraftDto>()
-                .ForMember(dest => dest.Times, opt => opt.MapFrom(src => src.Times))
-                .ForMember(dest => dest.Squawks, opt => opt.MapFrom(src => src.Squawks))
-                .ReverseMap();
-            // Times -> TimesDto
-            CreateMap<Times, TimesDto>()
-                .ReverseMap();
-            // Squawk -> SquawkDto
-            CreateMap<Squawk, SquawkDto>()
-                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
-                .ReverseMap();
-            // Status -> StatusDto
-            CreateMap<Status, StatusDto>()
-                .ReverseMap();
         }
     }
 }
--- CAM.Web/Startup.cs
+++ CAM.Web/Startup.cs
@@ -36,17 +36,6 @@ namespace CAM.Web
                 ));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.ConfigureSwaggerGen(options => options.CustomSchemaIds(x => x.FullName));
-            services.AddSwaggerGen(options =>
-                options.SwaggerDoc("v1", new Info { Title = "Centennial Aircraft Maintenance API", Version = "v1" }));
-            
-            // Temporary loop reference handling exception for troubleshooting
-            services.AddMvc().AddJsonOptions(options =>
-            {
-                options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
-                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
-                options.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,18 +50,12 @@ namespace CAM.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 // app.UseHsts();
             }
-            // OpenAPI
-            app.UseSwagger();
-            app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "Centennial Aircraft Maintenance"));
-
             // app.UseHttpsRedirection();
-            app.UseMvc();
-
-            // Run with OpenAPI redirect
-            app.Run(context =>
+            app.UseMvc(routes =>
             {
-                context.Response.Redirect("/swagger");
-                return Task.CompletedTask;
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
