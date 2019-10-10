b72262d3eff68c5e821ddf4e3e421f405591905b
Perustaja <chrishansen1111@gmail.com>
2019-10-10 15:00:45 -0600

    Revert "Revert "Restructure for Onion Architecture""
    This reverts commit 077246191f541a92aa13c68e5991cfe3a3a64681.

---
+++ CAM.Web/Areas/Aircraft/Controllers/AircraftController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CAM.Core.Entities;
+using CAM.Infrastructure.Data;
+using AutoMapper;
+
+namespace CAM.Web.Controllers
+{
+    [Area("Aircraft")]
+    public class AircraftController : ControllerBase
+    {
+    }
+}
--- CAM.Web/Startup.cs
+++ CAM.Web/Startup.cs
@@ -53,9 +53,14 @@ namespace CAM.Web
             // app.UseHttpsRedirection();
             app.UseMvc(routes =>
             {
+                // Add Area routing convention
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                name: "MyArea",
+                template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                   name: "default",
+                   template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
