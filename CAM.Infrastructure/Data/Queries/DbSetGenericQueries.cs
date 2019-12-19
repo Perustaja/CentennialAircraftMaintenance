3c6edcddc0820f75b7fad263d6d10880de4e5da0
Perustaja <chrishansen1111@gmail.com>
2019-12-19 12:03:45 -0700

    Initial query/command structure

--- CAM.Infrastructure/Data/Queries/DbSetGenericQueries.cs
+++ CAM.Infrastructure/Data/Queries/DbSetGenericQueries.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using CAM.Core.Interfaces;
 using CAM.Core.SharedKernel;
@@ -9,11 +11,39 @@ namespace CAM.Infrastructure.Data.DbSetExtensions
 {
     public static class DbSetGenericQueries
     {
-        public async static Task<T> GetByIdAsync<T, U>(this DbSet<T> set, U id)
+        /// <summary>
+        /// Performs a lookup, returning the first element with an id matching the given id, or default.
+        /// </summary>
+        public async static Task<T> GetByIdAsync<T, U>(this DbSet<T> set, U id, bool inclTracking = false)
         where T : BaseEntity<U>
         {
-            return await set.Where(e => e.Id.Equals(id)).FirstOrDefaultAsync();
+            if (inclTracking)
+                return await set.Where(e => e.Id.Equals(id)).FirstOrDefaultAsync();
+            else
+                return await set.Where(e => e.Id.Equals(id)).AsNoTracking().FirstOrDefaultAsync();
+
         }
-        
+        /// <summary>
+        /// Returns all of the entities from the DbSet as a List.
+        /// </summary>
+        public async static Task<List<T>> GetListAllAsync<T>(this DbSet<T> set, bool inclTracking = false)
+        where T : BaseEntity<string>
+        {
+            if (inclTracking)
+                return await set.ToListAsync();
+            else
+                return await set.AsNoTracking().ToListAsync();
+        }
+        /// <summary>
+        /// Performs a lookup, returning the first element with an id containing the given string, or default.
+        /// </summary>
+        public async static Task<List<T>> GetListAsync<T>(this DbSet<T> set, string exp, bool inclTracking = false)
+        where T : BaseEntity<string>
+        {
+            if (inclTracking)
+                return await set.Where(e => e.Id.Contains(exp)).ToListAsync();
+            else
+                return await set.Where(e => e.Id.Contains(exp)).AsNoTracking().ToListAsync();
+        }
     }
 }
